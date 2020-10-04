1a11cbc767687d7e9ea75a95d52ca36ef507d7ed
Andra Simion <andra.simion@info.uaic.ro>
2020-10-04 18:56:46 +0300

    first commit

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public GameObject robot;
+    public GameObject arCamera;
+    private Animator robotAnimator;
+    private bool happy = false;
+
+    void Start()
+    {
+        robotAnimator = robot.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float currentDist = Vector3.Distance(arCamera.transform.position, robot.transform.position);
+        
+        if (currentDist < 400 && !happy)
+        {
+            happy = true;
+            robotAnimator.SetBool("normal", false);
+            robotAnimator.SetBool("happy", true);
+        }
+        if(currentDist >= 400 && happy)
+        {
+            happy = false;
+            robotAnimator.SetBool("happy", false);
+            robotAnimator.SetBool("normal", true);
+        }
+    }
+}
